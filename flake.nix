9867040045e4d00e67b22505366c567219a8fc47
nick-ccc <nicholas.mastand@gmail.com>
2026-05-05 21:16:16 -0400

    updated flake for OS agnostic work

--- flake.nix
+++ flake.nix
@@ -5,28 +5,44 @@
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
   };
 
-  outputs = { self, nixpkgs }: let 
-    pkgs = nixpkgs.legacyPackages."x86_64-linux";
-  in {
-    devShells."x86_64-linux".default = pkgs.mkShell {
-      buildInputs = with pkgs; [
-        # main rust dependencies
-        cargo 
-        rustc 
-        rustfmt 
-        clippy 
-        rust-analyzer
-
-        # cargo tools
-        cargo-watch
-        cargo-edit
-        cargo-tarpaulin
-        cargo-audit
-
-        glib
-        pkg-config
+  outputs = { self, nixpkgs }:
+    let
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
       ];
-      RUST_SRC_PATH = "${pkgs.rustPlatform.rustLibSrc}";
+
+      forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f system);
+
+    in {
+      devShells = forAllSystems (system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in {
+          default = pkgs.mkShell {
+            buildInputs = with pkgs; [
+              # main rust dependencies
+              cargo
+              rustc
+              rustfmt
+              clippy
+              rust-analyzer
+
+              # cargo tools
+              cargo-watch
+              cargo-edit
+              cargo-tarpaulin
+              cargo-audit
+
+              glib
+              pkg-config
+            ];
+
+            RUST_SRC_PATH = "${pkgs.rustPlatform.rustLibSrc}";
+          };
+        }
+      );
     };
-  };
 }
